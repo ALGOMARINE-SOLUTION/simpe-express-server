d64940feb1bbb9de49bbbe6762c7490364e715f9
ErdemGhifuri <kesukaan.ultra@gmail.com>
2022-07-17 20:47:03 +0700

    exercise 1

--- src/Routing/index.ts
+++ src/Routing/index.ts
@@ -8,7 +8,51 @@ import { Router } from "express";
 const route = Router();
 
 // ALL ROUTES
-// TODO : Create routes
+// ELSA
+// TODO : API ELSA -> Mengambil list buku perpustakaan
+/*
+path: '/book-list'
+method: GET
+{
+  data : [
+    {
+      nama: 'Laskar Pelangi',
+      pengarang: 'Andrea Hirata'
+    },
+    {
+      nama: 'Divergent',
+      pengarang : 'Veronica Roth'
+    }
+  ]
+}
+*/
+
+// MOSES
+// TODO : API MOSES -> detail buku
+/*
+path: '/book-detail'
+method: GET
+{
+  data: {
+    nama: 'divergent',
+    pengarang : 'Veronica Roth',
+    tahun: 2011,
+    kota: 'New York',
+    penerbit: 'Mizan Fantasy'
+  }
+}
+*/
+
+// SALMAA
+// TODO : API SALMAA
+/*
+path: '/book-abstract'
+method: GET
+{
+  data: 'Buku ini menjelaskan tentang .....'
+}
+*/
+
 
 // =================================================================================================
 //                                            OTHERS
