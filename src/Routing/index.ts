2760d728c6f8361e417a4bb72fa7e554a53d0b31
salmaabayrus <salmaabayrus@gmail.com>
2022-07-17 20:52:10 +0700

    coba 1

--- src/Routing/index.ts
+++ src/Routing/index.ts
@@ -9,6 +9,12 @@ const route = Router();
 
 // ALL ROUTES
 // TODO : Create routes
+// route.get('/warna-warna', function (req, res) {
+//     const warna = ['kuning', 'hijau'];
+//     res.json ({ "data": warna }) ;
+// })
+// route.get('/buku-abstrak', function (req, res))
+
 
 // =================================================================================================
 //                                            OTHERS
