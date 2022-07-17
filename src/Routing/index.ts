7a96e60c18e9813b807b99ed8b34daa1698019da
salmaabayrus <salmaabayrus@gmail.com>
2022-07-17 21:01:24 +0700

    add book abstract api

--- src/Routing/index.ts
+++ src/Routing/index.ts
@@ -13,8 +13,55 @@ const route = Router();
 //     const warna = ['kuning', 'hijau'];
 //     res.json ({ "data": warna }) ;
 // })
-// route.get('/buku-abstrak', function (req, res))
 
+// ELSA
+// TODO : API ELSA -> Mengambil list buku perpustakaan
+/*
+path: '/book-list'
+method: GET
+{
+  data : [
+    {
+      nama: 'Laskar Pelangi',
+      pengarang: 'Andrea Hirata'
+    },
+    {
+      nama: 'Divergent',
+      pengarang : 'Veronica Roth'
+    }
+  ]
+}
+*/
+
+// MOSES
+// TODO : API MOSES -> detail buku
+/*
+path: '/book-detail'
+method: GET
+{
+  data: {
+    nama: 'divergent',
+    pengarang : 'Veronica Roth',
+    tahun: 2011,
+    kota: 'New York',
+    penerbit: 'Mizan Fantasy'
+  }
+}
+*/
+
+// SALMAA
+// TODO : API SALMAA
+/*
+path: '/book-abstract'
+method: GET
+{
+  data: 'Buku ini menjelaskan tentang .....'
+}
+*/
+route.get('/book-abstract', function (req, res) {
+    const summary = "Buku ini menjelaskan tentang dunia dengan 5 fraksin dan ..."
+    res.json ({ "data": summary })
+})
 
 // =================================================================================================
 //                                            OTHERS
