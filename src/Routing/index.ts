3423e013595f0f4e6c003b12acf568b183c57ffc
unknown <moseskurniawan2002@gmail.com>
2022-07-17 21:58:16 +0800

    api detail buku

--- src/Routing/index.ts
+++ src/Routing/index.ts
@@ -8,7 +8,40 @@ import { Router } from "express";
 const route = Router();
 
 // ALL ROUTES
+// '/class mobil {
+//     brand: strring;
+//     warna: string;
+//     constructor() {
+//         this.brand = 'daihatsu'
+//         this.warna = 'merah'
+//     }
+// }
+
+// const mobil = new mobil()
+// mobil.warna
+// mobil.startermobil
+
+
+// route.get ('/jenis-warna', function (req, res){
+//     const warna = ["merah", "hitam"]
+//     res.send(warna)
+// })
+
+
+
 // TODO : Create routes
+route.get ('/detail-buku', function (req, res){
+    const buku = {
+        nama: 'divergent',
+        pengarang : 'Veronica Roth',
+        tahun: 2011,
+        kota: 'New York',
+        penerbit: 'Mizan Fantasy'
+  }
+    res.send(buku)
+})
+
+
 
 // =================================================================================================
 //                                            OTHERS
