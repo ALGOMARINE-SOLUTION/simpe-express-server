b2890ccd71c5863cc29b21d91ce5f7078c07945d
unknown <moseskurniawan2002@gmail.com>
2022-07-30 20:35:38 +0700

    merapikan logika segitiga

--- src/Controllers/SegitigaController.ts
+++ src/Controllers/SegitigaController.ts
@@ -1,4 +1,5 @@
 import Joi from "joi";
+import { hitungLuasSegitigaService } from "../Services/SegitigaService";
 
 export function hitungLuasSegitiga(req: any, res: any) {
 //Jurus validasi
@@ -8,24 +9,10 @@ const skemaValidasi = Joi.object ({
 })
 const hasilValidasi = skemaValidasi.validate(req.body);
 if(!hasilValidasi.error) {
-
-  let luas = 0
-  let keliling = 0
-  // LOGIKA LUAS DAN KELILING SEGITIGA SAMA SISI
-  //1 ambil data dari client/user
-  const payload = req.body;
-  //2 definisikan variabel yang dibutuhkan
-  const alas = payload.alas
-  const tinggi = payload.tinggi
-
-  //3 hitung luas
-  luas = alas*tinggi/2;
-  //4 hitung keliling
-  keliling = 3*alas
-  // TODO : Membuat logika untuk menghitung luas dan keliling segitiga
+const data =  hitungLuasSegitigaService(req.body)
 
   // Kirim Luas dan Keliling ke User
-  return res.json({ data: { luas: luas, keliling: keliling } })
+  return res.json({ data: data })
 } else {
     console.log(hasilValidasi.error)
     res.status(400).json({ error: 'anda salah' })
