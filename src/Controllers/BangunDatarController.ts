db2594126110fc3df464f3c8577cc469594088a9
unknown <cacasalsabilaap@gmail.com>
2022-07-30 20:58:04 +0700

    bangun datar controller dan service

--- src/Controllers/BangunDatarController.ts
+++ src/Controllers/BangunDatarController.ts
@@ -18,6 +18,24 @@ class BangunDatarController {
       res.status(400).json({ error: 'datanya salah' })
     }
   }
+
+  hitungLuasLingkaran (req: any, res: any) {
+    //=======VALIDASI=======
+    const skemaValidasi = Joi.object({
+        radius: Joi.number().required()
+    })
+    
+    const hasilValidasi = skemaValidasi.validate(req.body);
+    if(!hasilValidasi.error) {
+    const data = bangunDatarService.hitungLuasLingkaranService(req.body)
+    
+    // Kirim Luas dan Keliling ke User
+    return res.json({ data: data })
+    } else {
+        console.log(hasilValidasi.error)
+        res.status(400).json({ error: 'salah bro'})
+    }
+  }
 }
 
 export const bangunDatarController = new BangunDatarController()
