c84347f54b2b0f4b8af09375425b125b2519f4de
ErdemGhifuri <kesukaan.ultra@gmail.com>
2022-08-06 22:50:14 +0700

    adding database

--- src/Helper/db.ts
+++ src/Helper/db.ts
@@ -1,6 +1,14 @@
-async function query(sql, params) {
-    const connection = await mysql.createConnection(config.db);
-    const [results, ] = await connection.execute(sql, params);
-  
-    return results;
-  }
+import mysql from "mysql";
+
+export async function query(sql: string, params: string[]) {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'diklat_database',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+  });
+
+  const results = connection.query(sql, params);
+
+  return results;
+}
